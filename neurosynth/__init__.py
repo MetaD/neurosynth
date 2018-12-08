89f061f9c402ef4baeebea602d096900fc5c5f03
Meng Du <mengdu@umich.edu>
2018-12-08 14:52:04 -0800

    fix init

--- neurosynth/__init__.py
+++ neurosynth/__init__.py
@@ -8,11 +8,9 @@ __all__ = ["analysis", "base", "set_logging_level", "__version__"]
 # Easier access to key modules/classes
 from .base.dataset import Dataset
 from .base.mask import Masker
-from .analysis.cluster import Clusterable
 from .analysis.meta import MetaAnalysis
-from .analysis.decode import Decoder
 from .base import dataset, imageutils, lexparser, mask, transformations
-from .analysis import classify, cluster, decode, meta, network, reduce
+from .analysis import meta, network, reduce
 
 import logging
 import sys
